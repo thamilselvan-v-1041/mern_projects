b50a2dd2703e0bdfb39419722abdfdc456ae9bbc
thamilselvan-v-1041 <thamilselvan.v@zohocorp.com>
2026-02-04 17:02:42 +0530

    Update database.ts

--- todo-list/backend/src/config/database.ts
+++ todo-list/backend/src/config/database.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 export const connectDatabase = async (): Promise<void> => {
   try {
-    const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/todo-list';
+    const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://thamilselvanv_db_user:<db_password>@webserver.ewtkhfx.mongodb.net/?appName=WebServer';
+      //'mongodb://localhost:27017/todo-list';
     await mongoose.connect(mongoUri);
     console.log('MongoDB connected successfully');
   } catch (error) {
